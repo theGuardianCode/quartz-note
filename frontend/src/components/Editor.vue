782bd8edc37b3fbb3247c570999c61a9604bf36b
Nick Rose <nickrichard878@gmail.com>
2025-04-09 21:10:18 +1000

    Super basic protoype. Type and show in p tag.

---
+++ frontend/src/components/Editor.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { EventsOn } from '../../wailsjs/runtime/runtime';
+
+import { ref } from 'vue'
+
+let editing = ref(false);
+let input = ref("");
+
+function toggle() {
+  editing.value = !editing.value;
+}
+
+EventsOn("toggle-edit", toggle);
+</script>
+
+<template>
+  <div class="container">
+    <div class="pane">
+      <textarea v-if="editing" v-model="input"></textarea>
+      <p v-else>{{ input }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+textarea {
+  display: block;
+  border: none;
+  outline: none;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 98%;
+  padding: 5px;
+}
+
+.pane {
+  height: 100%;
+}
+
+.pane p {
+  overflow-wrap: break-word;
+}
+</style>
--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"embed"
+	gr "runtime"
 
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/menu"
+	"github.com/wailsapp/wails/v2/pkg/menu/keys"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 //go:embed all:frontend/dist
@@ -15,6 +19,20 @@ func main() {
 	// Create an instance of the app structure
 	app := NewApp()
 
+	AppMenu := menu.NewMenu()
+	if gr.GOOS == "darwin" {
+		AppMenu.Append(menu.AppMenu())
+	}
+
+	FileMenu := AppMenu.AddSubmenu("File")
+	FileMenu.AddText("Toggle", keys.CmdOrCtrl("t"), func(_ *menu.CallbackData) {
+		runtime.EventsEmit(app.ctx, "toggle-edit")
+	})
+
+	if gr.GOOS == "darwin" {
+		AppMenu.Append(menu.EditMenu())
+	}
+
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "quartz-note",
@@ -23,8 +41,9 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 255, G: 255, B: 255, A: 1},
 		OnStartup:        app.startup,
+		Menu:             AppMenu,
 		Bind: []interface{}{
 			app,
 		},
